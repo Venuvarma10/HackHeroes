f5a3896b3a2d407503d39f1c1ee3c99b04f7a6c6
umasankar <umasankarvaradati@gmail.com>
2025-03-27 18:53:35 +0530

    Auto-sync update

--- HackHeros/EcoFinAI/views.py
+++ HackHeros/EcoFinAI/views.py
@@ -36,6 +36,6 @@ class Evalute(APIView):
     permission_classes=[AllowAny]
     def post(self, request):
         data = request.data
-        predict_project
+       predicted_data= predict_project
         # serializer = EvaluteSerializer(data=data)
         return Response({'message':data})
