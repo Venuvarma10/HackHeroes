3f4dda4a688e757df4111a385b3f2fff6c8a82ab
umasankar <umasankarvaradati@gmail.com>
2025-03-27 16:50:22 +0530

    Auto-sync update

--- HackHeros/EcoFinAI/views.py
+++ HackHeros/EcoFinAI/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.response import Response\
-from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated, AllowAny
 from .serializers import RegistrationSerializer
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -25,3 +26,15 @@ class Registration(APIView):
                 }, status=201)
         return Response(serializer.errors, status=400)
 
+class Login(APIView):
+    permission_classes=[AllowAny]
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            # token, created = Token.objects.get_or_create(user=user)
+            return Response({"name": user.get_full_name(),
+                            #  "token": token.key
+                             }, status=202)
+        return Response({"message": "Invalid
