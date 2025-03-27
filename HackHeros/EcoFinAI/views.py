9f27ebee5708a5b3a7b2f6f3377ef932d5cbec1a
umasankar <umasankarvaradati@gmail.com>
2025-03-27 22:06:27 +0530

    Auto-sync update

--- HackHeros/EcoFinAI/views.py
+++ HackHeros/EcoFinAI/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import AllowAny,IsAuthenticated
 from .finml import predict_project
 
 from google import genai
+def suggestions()
 
 client = genai.Client(api_key="AIzaSyDUaHL8CI0P6ukndFVCVdxzs4qkWWevPNU")
 
