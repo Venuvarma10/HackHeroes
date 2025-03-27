40a36778738fc11e3dc4de5e412a456333a6c5d7
umasankar <umasankarvaradati@gmail.com>
2025-03-27 18:27:26 +0530

    Auto-sync update

--- HackHeros/api/urls.py
+++ HackHeros/api/urls.py
@@ -1,10 +1,6 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from EcoFinAI.views import Registration,Login
-
 from django.urls import path
 urlpatterns = [
     path('register/',Registration.as_view()),
     path('login/',Login.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login (get token)
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
 ]
