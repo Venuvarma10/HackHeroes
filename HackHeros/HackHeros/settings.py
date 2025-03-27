df76a7f0bcd6d9072890f1edf335909a3d1e86d7
umasankar <umasankarvaradati@gmail.com>
2025-03-27 18:22:45 +0530

    Auto-sync update

--- HackHeros/HackHeros/settings.py
+++ HackHeros/HackHeros/settings.py
@@ -133,7 +133,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.Tokenauthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated', 
