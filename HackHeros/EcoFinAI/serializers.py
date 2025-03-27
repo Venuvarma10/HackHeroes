5e724edc601bfae10238ec5bb5f51e9b84230622
umasankar <umasankarvaradati@gmail.com>
2025-03-27 18:31:43 +0530

    Auto-sync update

--- HackHeros/EcoFinAI/serializers.py
+++ HackHeros/EcoFinAI/serializers.py
@@ -1,19 +1,26 @@
-from rest_framework.serializers import Serializer
-
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username','email','password']
+        fields = ['id', 'username', 'email', 'password']
 
-    
-    def create(self,validated_data):
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists.")
+        return value
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        return value
+
+    def create(self, validated_data):
         user = User.objects.create(
-            username = validated_data['username'],
-            email = validated_data['email']        
+            username=validated_data['username'],
+            email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
