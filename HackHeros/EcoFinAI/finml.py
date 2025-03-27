2fcda9ca75eb5f2b3708c1ada5035f1a464f144d
umasankar <umasankarvaradati@gmail.com>
2025-03-27 19:04:31 +0530

    Auto-sync update

--- HackHeros/EcoFinAI/finml.py
+++ HackHeros/EcoFinAI/finml.py
@@ -110,6 +110,7 @@ y_capital_pred = capital_model.predict(X_test)
 # Function to Predict New Project
 
 def predict_project(location, category, initial_capital):
+    print("debug")
     location_encoded = label_encoders['location'].transform([location])[0]
     category_encoded = label_encoders['category'].transform([category])[0]
     features = np.array([[location_encoded, category_encoded, initial_capital]])
